87e5904f8acd947326a83b4fe22493f9cba7c8a2
SeniorJack <45121264+ob-cell@users.noreply.github.com>
2025-01-08 19:09:08 +0100

    Update script.js
    Major Update

--- script.js
+++ script.js
@@ -12,19 +12,42 @@ firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
 
 const username = prompt("What's your username oomf?");
+const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+const coloredUsername = `<span style="color:${randomColor}">${username} (${randomColor})</span>`;
 
-document.getElementById("send-message").addEventListener("submit", postChat); function postChat(e) 
-{ e.preventDefault();
-const timestamp = Date.now(); 
-const chatTxt = document.getElementById("chat-txt");
-const message = chatTxt.value; chatTxt.value = ""; 
-db.ref("messages/" + timestamp).set({ 
-  usr: username,
-  msg: message, 
+document.getElementById("send-message").addEventListener("submit", postChat); 
+
+function postChat(e) { 
+  e.preventDefault();
+  const timestamp = Date.now(); 
+  const chatTxt = document.getElementById("chat-txt");
+  const message = chatTxt.value; 
+  chatTxt.value = ""; 
   
-}); 
+  if (message === "!help") {
+    db.ref("messages/" + timestamp).set({ 
+      usr: "sYs (bot)",
+      msg: "<i style='color:gray'>someone used the !help command</i> Hi, I'm sYs" 
+    });
+  } else {
+    db.ref("messages/" + timestamp).set({ 
+      usr: coloredUsername,
+      msg: message, 
+    }); 
+  }
+
+  scrollToBottom();
 }
+
 const fetchChat = db.ref("messages/"); 
-fetchChat.on("child_added", function (snapshot) { const messages = snapshot.val(); 
-const msg = "<li>" + messages.usr + " : " + messages.msg + "</li>";
-document.getElementById("messages").innerHTML += msg; });
+fetchChat.on("child_added", function (snapshot) { 
+  const messages = snapshot.val(); 
+  const msg = "<li>" + messages.usr + " : " + messages.msg + "</li>";
+  document.getElementById("messages").innerHTML += msg; 
+  scrollToBottom();
+});
+
+function scrollToBottom() {
+  const chatBox = document.querySelector(".chat");
+  chatBox.scrollTop = chatBox.scrollHeight;
+}
